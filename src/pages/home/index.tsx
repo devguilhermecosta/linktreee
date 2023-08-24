def89f1927af121c7eef460bc2bcf695a0714862
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-08-24 00:18:45 -0300

    update: updated the home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,18 @@
+import './home.modules.css'
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      <main className='C-main'>
+        <h1>Dev Guilherme Costa </h1>
+        <p>Veja meus links 👇</p>
+
+        <section className='C-links'>
+          <a href="">
+            <p>Canal do Youtube</p>
+          </a>
+        </section>
+      </main>
     </div>
   )
 }
