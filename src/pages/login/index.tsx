2c19e134f6d261407dd9807ca88c23f3ead386bb
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-08-27 11:31:01 -0300

    style: changed the devlink span style

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,15 @@
+import { Link } from 'react-router-dom'
 import './login.modules.css'
 
 export default function Login() {
   return (
     <section className="C-login">
-      <h1>
-        Dev
-        <span className="C-login_logo_span">Link</span>
-      </h1>
+      <Link to="/">
+        <h1>
+          Dev
+          <span className="C-login_logo_span">Link</span>
+        </h1>
+      </Link>
     </section>
   )
 }
