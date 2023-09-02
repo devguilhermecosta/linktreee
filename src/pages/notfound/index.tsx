5735062b319dc46cedf4b117c163676e730348f3
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-09-01 22:54:49 -0300

    update: styled the not found page

--- src/pages/notfound/index.tsx
+++ src/pages/notfound/index.tsx
@@ -1,7 +1,16 @@
+import './notfound.modules.css';
+import { Link } from 'react-router-dom';
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page not found</h1>
+    <div className="C-notfound_page">
+      <h1>404</h1>
+      <h2>Página não encontrada</h2>
+      <p>Você caiu em uma página que não existe.</p>
+
+      <Link to="/">
+        voltar para a home
+      </Link>
     </div>
   )
 }
